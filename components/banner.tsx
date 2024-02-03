b3885550075f017d384428bc5f23f0f23c0a5e1c
maryaynes <119408028+maryaynes@users.noreply.github.com>
2024-02-03 01:33:32 +0000

    updated banner display

--- components/banner.tsx
+++ components/banner.tsx
@@ -6,46 +6,44 @@ import {
   Stack,
   Text,
   Link,
-  useBreakpointValue,
 } from '@chakra-ui/react';
 
 export default function Banner() {
   return (
-    <Stack maxH={'40vh'} direction={{ base: 'column', md: 'row' }}>
+    <Stack direction={{ base: 'column', md: 'row' }} spacing={4} maxH={{ base: 'unset', md: '40vh' }}>
       <Flex p={8} flex={1} align={'center'} justify={'center'}>
         <Stack spacing={6} w={'full'} maxW={'lg'}>
           <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
             <Text fontSize="3xl" textAlign="center" color="accent">
-              Verifica o teu posicionamento de acordo com a visão que tens da
-              sociedade.
+              Verifica o teu posicionamento de acordo com a visão que tens da sociedade.
             </Text>
           </Heading>
-          <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
+          <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'} textAlign="center">
             Descobre com que partido fazes match.
           </Text>
-          <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
+          <Stack direction={{ base: 'column', md: 'row' }} spacing={4} justifyContent={'center'} align={'center'}>
             <Link href="/votation">
-            <Button
-              rounded={'full'}
-              bg={'#5966B3'}
-              color={'white'}
-              _hover={{
-                bg: '#5966C6',
-              }}>
-              Testar Agora!
-            </Button>
+              <Button
+                rounded={'full'}
+                bg={'#5966B3'}
+                color={'white'}
+                _hover={{
+                  bg: '#5966C6',
+                }}>
+                Testar Agora!
+              </Button>
             </Link>
           </Stack>
         </Stack>
       </Flex>
-      <Flex flex={1}  overflow={'hidden'}>
+      <Flex flex={1} overflow={'hidden'}>
         <Image
           alt={'Login Image'}
           objectFit={'cover'}
-          maxH={'80%'} // Set maximum height
-          maxW={'100%'} // Set maximum width
+          maxH={{ base: '50vh', md: '100%' }} // Adjusted max height for responsiveness
+          maxW={'100%'}
           src={
-            'https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+            'votacao_img.jpg'
           }
         />
       </Flex>
