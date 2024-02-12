f51c79e63434d01900993cca9776324daf6ff263
maryaynes <119408028+maryaynes@users.noreply.github.com>
2024-02-12 20:08:55 +0000

    fix: minor improvements

--- pages/votation.tsx
+++ pages/votation.tsx
@@ -60,6 +60,13 @@ export default function Votation() {
   const currentIdea: () => Idea = () => ideas[ideaIndex];
 
   const startQuiz = () => {
+    // Checking if at least one subject is selected
+    if (subjects.length === 0) {
+      // prompt mensagem pop up
+      alert('Escolha pelo menos um tema para iniciar o quiz.');
+      return;
+    }
+
     getVotation(subjects).then((data) => {
       setIdeas(data);
       setShowVotationForm(true);
@@ -95,7 +102,6 @@ export default function Votation() {
             textAlign="center"
             flex="2"
             bgColor="tier"
-            marginTop="50px"
             w="100vw"
           >
             <Heading
