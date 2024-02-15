f2c3e1d74220d652a6ee838a981245bd717fd8b7
Tiago Aguiar <tiagoaguiar100@gmail.com>
2024-02-15 11:59:04 +0000

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,19 @@
-import {ChakraProvider} from '@chakra-ui/react';
-import {AppWrapper} from '../context/AppContext';
+import '../styles/globals.css';
+
 import {
-  Chart as ChartJS,
   ArcElement,
-  Tooltip,
-  Legend,
-  RadialLinearScale,
-  PointElement,
-  LineElement,
+  Chart as ChartJS,
   Filler,
+  Legend,
+  LineElement,
+  PointElement,
+  RadialLinearScale,
+  Tooltip,
 } from 'chart.js';
 
-import '../styles/globals.css';
+import {AppWrapper} from '../context/AppContext';
+import {ChakraProvider} from '@chakra-ui/react';
+import Script from 'next/script';
 import theme from '../styles/theme';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
@@ -23,9 +25,27 @@ ChartJS.register(
   Tooltip,
   Legend,
 );
+
+function TrackingCode() {
+  return (
+    <>
+  <Script async src="https://www.googletagmanager.com/gtag/js?id=G-Q4RC1SY1JG"/>
+  <Script id="script-gtm" strategy="afterInteractive">
+    {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){window.dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-GOOGLEID');
+    `}
+  </Script>
+  </>)
+}
+
 function MyApp({Component, pageProps}) {
   return (
     <AppWrapper>
+      <TrackingCode />
       <ChakraProvider theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
